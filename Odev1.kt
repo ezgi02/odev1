eb2d4d9544a21f41c55f70157260f6eba35a5039
Ezgi Adıgüzel <ezgiadiguzel01@gmail.com>
2024-12-05 12:28:15 +0300

    ilk odev gonderimi

---
+++ Odev1.kt
@@ -0,0 +1,74 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main(){
+    val ilce="Bahcelievler"
+    println(ilce)
+    val kıta="Avrupa"
+    println("Fransa $kıta 'sında yer alan ulkelerden biri")
+    val faks="+90 212 555 1234."
+    println("Faks numarası $faks ")
+    val postaKodu=34000
+    println("İstanbul un merkezi bir posta kodu $postaKodu")
+    var instaAdresi ="@gezegenseyyah"
+    println("işletmenin insta adresi $instaAdresi ")
+    val urunMiktari=18
+    val calistiginbolum="Bilgisayar Muhendisligi"
+    println("onun calistigi bolum $calistiginbolum dir")
+    val musteriSoyadi="Akkaya"
+    val odemeMiktari=1850.7587
+    println("$musteriSoyadi soyadlı odemesi gereken miktar $odemeMiktari ")
+    val dogumTarihi="31/05/2001"
+    val medeniHal="bekar"
+    val videoYorumu="o video çok komikti"
+    val ödemeSaati="17.50"
+    val satilanMiktar=1450
+    val telefonModeli="iPhone 15 Pro."
+    val dergiAdi="Vogue"
+    val basimTarihi="Ekim 2024"
+    println("$dergiAdi dergisi $basimTarihi tarihinde çıkacak")
+    val zamMiktari=40
+    println("iscilere yapılacak zam miktari % $zamMiktari")
+    val daireSayisi=8
+    val enlem=30
+    val boylam=60
+    val yemek_adi="kebab"
+    val urunFiyati=198
+    println("$yemek_adi fiyati $urunFiyati")
+    val muzikSuresi=40
+    println("muzik suresi $muzikSuresi")
+    val mekan_puani=7.5
+    println("Yeni açilan mekanin puani $mekan_puani")
+    val dosyaAdi="tekardersleri"
+    println(dosyaAdi)
+    val resimFormati="jpeg"
+    println("resmin formati $resimFormati ")
+    val renk="siyah"
+    println("Elbisenin rengi $renk")
+    val renk_kodu="#3498db"
+    println("bu renk kodu $renk_kodu, açık mavi bir renk kodudur")
+    val bilgisayarModeli="MacBook Pro "
+    val ekranBoyutu="16-inch"
+    println("Bilgisayarin modeli $bilgisayarModeli ekran boyutu $ekranBoyutu")
+    val ekranSuresi=4
+    val basinc="1013hPa"
+    println("basinci"+ " "+basinc)
+    val etkinlikgunu="pazartesi"
+    println("etkinlik günü"+ " "+etkinlikgunu)
+    val odemegunu="carsamba"
+    val yolculukCikisTarihi="persembe"
+    println(yolculukCikisTarihi)
+    val mahelleAdi="Namık kemal"
+    println(mahelleAdi)
+    val kullanilanDakika=40
+    val kargoTakipNo=145972368
+    println("kargo takipno $kargoTakipNo")
+    val kuponSuresi=45
+    val kuponKodu="CZXL25"
+    println("yüzde 25 indirimiin kupon kodu $kuponKodu")
+    val faturaTarihi="15/08/2024"
+
+
+
+
+
+}
